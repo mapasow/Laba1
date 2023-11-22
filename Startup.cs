e0989eaa2727deee7ed2ce144dd8dcc65715e161
mapasow <151778175+mapasow@users.noreply.github.com>
2023-11-22 22:18:34 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CRUDADO
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // Значение HSTS по умолчанию — 30 дней. Возможно, вы захотите изменить это для производственных сценариев, см. https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,29 @@
+﻿using CRUDADO.CustomValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDADO.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [SkillsValidate(Allowed = new string[] { "История", "Химия", "Обществознание", "География", "Геометрия", "Алгебра", "Русский язык", "Физика" }, ErrorMessage = "Ввести можно только школьный предмет")]
+        public string Skills { get; set; }
+
+        [Range(5, 50)]
+        public int TotalStudents { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+
+        public DateTime AddedOn { get; set; }
+    }
+}
